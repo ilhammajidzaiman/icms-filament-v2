ed17cfe0acd23e0ddcf4208a92cba9f8f7ae8a23
ilham majid zaiman <ilhammajidzaiman07@gmail.com>
2023-08-07 21:23:08 +0700

    refactor: user resource

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources;
 
+use stdClass;
 use Filament\Forms;
 use App\Models\User;
 use Filament\Tables;
@@ -9,10 +10,16 @@ use Filament\Resources\Form;
 use Filament\Resources\Table;
 use Filament\Resources\Resource;
 use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Toggle;
+use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Contracts\HasTable;
 use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Forms\Components\FileUpload;
+use Filament\Tables\Columns\ToggleColumn;
 use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\DateTimePicker;
 use App\Filament\Resources\UserResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\UserResource\RelationManagers;
@@ -23,7 +30,9 @@ class UserResource extends Resource
     protected static ?string $modelLabel = 'User';
     protected static ?string $navigationIcon = 'heroicon-o-user';
     protected static ?string $navigationGroup = 'Admin';
-    protected static ?string $navigationLabel = 'User';
+    protected static ?string $navigationLabel = 'User 2';
+    protected static ?string $slug = 'user-2';
+    protected static ?int $navigationSort = 2;
     protected static ?string $recordTitleAttribute = 'name';
 
     public static function form(Form $form): Form
@@ -32,38 +41,105 @@ class UserResource extends Resource
             ->schema([
                 Card::make()
                     ->schema([
-                        TextInput::make('name')->required(),
-                        TextInput::make('email')->required(),
-                        TextInput::make('password')->required()->password(),
-                        FileUpload::make('image')
-                            ->directory('users')
+                        TextInput::make('name')
+                            ->label('Nama')
+                            ->required()
+                            ->maxLength(255),
+                        TextInput::make('email')
+                            ->label('Email')
+                            ->required()
+                            ->maxLength(255)
+                            ->email(),
+                        TextInput::make('password')
+                            ->password()
+                            ->required()
+                            ->maxLength(255),
+                    ])
+                    ->columnSpan(2),
+                Card::make()
+                    ->schema([
+                        Toggle::make('is_active')
+                            ->label('Status')
+                            ->required()
+                            ->default('1'),
+                        FileUpload::make('file')
+                            ->label('File')
+                            ->required()
+                            ->maxSize(1024)
+                            ->directory('users/' . date('Y/m'))
+                            ->image()
                             ->enableDownload()
-                            ->enableOpen()
-                        // ->multiple()
-                        // ->enableReordering()
-                        // ->getUploadedFileNameForStorageUsing(function (TemporaryUploadedFile $file): string {
-                        //     return (string) str($file->getClientOriginalName())->prepend(now() . '-');
-                        // })
+                            ->enableOpen(),
 
                     ])
-            ]);
+                    ->columnSpan(1),
+            ])
+            ->columns(3);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                TextColumn::make('name')->sortable()->searchable(),
-                TextColumn::make('email')->sortable()->searchable()
+                TextColumn::make('index')
+                    ->label('#')
+                    ->getStateUsing(
+                        static function (stdClass $rowLoop, HasTable $livewire): string {
+                            return (string) ($rowLoop->iteration +
+                                ($livewire->tableRecordsPerPage * ($livewire->page - 1
+                                ))
+                            );
+                        }
+                    ),
+                TextColumn::make('id')
+                    ->label('Id')
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                ImageColumn::make('file')
+                    ->label('File')
+                    ->circular(),
+                TextColumn::make('name')
+                    ->label('Nama')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->label('Email')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('email_verified_at')
+                    ->label('Diverifikasi')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('updated_at')
+                    ->label('Diperbarui')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('deleted_at')
+                    ->label('Dihapus')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                ToggleColumn::make('is_active')
+                    ->label('Status')
+                    ->sortable(),
             ])
             ->filters([
-                //
+                Tables\Filters\TrashedFilter::make(),
             ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
+            // ->actions([
+            //     Tables\Actions\EditAction::make(),
+            // ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
+                Tables\Actions\ForceDeleteBulkAction::make(),
+                Tables\Actions\RestoreBulkAction::make(),
             ]);
     }
 
@@ -82,5 +158,13 @@ class UserResource extends Resource
             'view' => Pages\ViewUser::route('/{record}'),
             'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,13 +5,14 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use SolutionForest\FilamentAccessManagement\Concerns\FilamentUser;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, FilamentUser;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes, FilamentUser;
 
     /**
      * The attributes that are mass assignable.
@@ -42,5 +43,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'is_active' => 'boolean',
     ];
 }
